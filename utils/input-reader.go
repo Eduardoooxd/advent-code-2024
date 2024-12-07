74bd92e9454844b4075248692250e73e7325beb2
Eduardoooxd <ecouto93@gmail.com>
2024-12-07 18:16:54 +0000

    improvements - day 2

---
+++ utils/input-reader.go
@@ -0,0 +1,31 @@
+package utils
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func ReadInput(filename string) []string {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error opening file: %v\n", err)
+		os.Exit(1)
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+		os.Exit(1)
+	}
+
+	return lines
+}
